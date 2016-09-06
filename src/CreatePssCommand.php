18e18b281e5056dfa5fa3c1e16674283bb5e9915
李铭昕 <715557344@qq.com>
2016-09-06 11:45:01 +0800

    * 新增密码服务

--- src/CreatePssCommand.php
+++ src/CreatePssCommand.php
@@ -1,6 +1,6 @@
 <?php
 // +----------------------------------------------------------------------
-// | CreateAjaxCommand [ 创建Ajax输出服务 ]
+// | CreatePssCommand [ 创建密码服务 ]
 // +----------------------------------------------------------------------
 // | Copyright (c) 2016 http://www.lmx0536.cn All rights reserved.
 // +----------------------------------------------------------------------
@@ -8,26 +8,25 @@
 // +----------------------------------------------------------------------
 // | Date: 2016/9/6 Time: 11:43
 // +----------------------------------------------------------------------
-
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class CreateAjaxCommand extends Command
+class CreatePssCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'make:limx-ajax';
+    protected $signature = 'make:limx-pss';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'create service AjaxResponse';
+    protected $description = 'create a pwd service';
 
     /**
      * Create a new command instance.
@@ -46,74 +45,85 @@ class CreateAjaxCommand extends Command
      */
     public function handle()
     {
-        $res = $this->createAjaxResponse();
+        $res = $this->createService();
         if ($res === false) return;
-        $res = $this->createAjaxResponseFacade();
+        $res = $this->createServiceFacade();
         if ($res === false) return;
         $this->info('Command execution success...');
     }
 
-    private function createAjaxResponseFacade()
+    private function createServiceFacade()
     {
         $root = app_path('Facades');
         if (!is_dir($root)) {
             mkdir($root, 0755, true);
         }
-        $file = $root . '/AjaxResponseFacade.php';
+        $file = $root . '/PwdSetServiceFacade.php';
         if (file_exists($file)) {
             $this->info($file . ' is exists!');
             return false;
         }
-        $content = '<?php namespace App\Facades;
+        $content = '<?php
+
+namespace App\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class AjaxResponseFacade extends Facade
+class PwdSetServiceFacade extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return \App\Services\AjaxResponse::class;
+        return \App\Services\PwdSetService::class;
     }
 }';
         file_put_contents($file, $content);
         return true;
     }
 
-    private function createAjaxResponse()
+    private function createService()
     {
         $root = app_path('Services');
         if (!is_dir($root)) {
             mkdir($root, 0755, true);
         }
-        $file = $root . '/AjaxResponse.php';
+        $file = $root . '/PwdSetService.php';
         if (file_exists($file)) {
             $this->info($file . ' is exists!');
             return false;
         }
-        $content = '<?php namespace App\Services;
-
-class AjaxResponse
+        $content = '<?php
+namespace App\Services;
+class PwdSetService
 {
-    protected function ajaxResponse($status = 1, $data = [], $message = \'\')
-    {
-        $out = [
-            \'status\' => $status,
-            \'message\' => $message,
-            \'data\' => $data,
-            \'timestamp\' => time()
-        ];
+    private $key = \'your sercet key\';
+    private $type = \'md5\';
 
-        return response()->json($out);
+    public function __construct()
+    {
+        //$this->key=config();
     }
 
-    public function success($data = null)
+    /**
+     * [pwd 用户名的名文密码转化为加密密码]
+     * @author limx
+     * @param $pwd 名文密码
+     * @return 密文密码
+     */
+    public function pwd($pwd)
     {
-        return $this->ajaxResponse(1, $data);
+        return md5($this->key . md5($pwd));
     }
 
-    public function fail($message, $extra = [])
+    /**
+     * [check 验证密码]
+     * @author limx
+     * @param $pwd 名文密码
+     * @param $seckey 密文密码
+     * @return bool
+     */
+    public function check($pwd, $seckey)
     {
-        return $this->ajaxResponse(0, $extra, $message);
+        return $this->pwd($pwd) === $seckey;
     }
 }';
 
