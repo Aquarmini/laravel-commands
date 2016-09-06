628759fab2bef6a41e77b70d482e45a3909fb482
李铭昕 <715557344@qq.com>
2016-09-06 15:31:06 +0800

    * 增加注释

--- tpl/ajax/AjaxResponse.php
+++ tpl/ajax/AjaxResponse.php
@@ -1,6 +1,6 @@
 <?php
 // +----------------------------------------------------------------------
-// | Demo [ WE CAN DO IT JUST THINK IT ]
+// | AjaxResponse [ WE CAN DO IT JUST THINK IT ]
 // +----------------------------------------------------------------------
 // | Copyright (c) 2016 http://www.lmx0536.cn All rights reserved.
 // +----------------------------------------------------------------------
@@ -12,7 +12,15 @@ namespace App\Services;
 
 class AjaxResponse
 {
-    protected function ajaxResponse($status = 1, $data = [], $message = '')
+    /**
+     * [ajaxResponse 返回特定格式的json]
+     * @author limx
+     * @param int $status 状态码
+     * @param array $data 数据包
+     * @param string $message 错误信息
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function ajaxResponse($status = 1, $data = [], $message = '')
     {
         $out = [
             'status' => $status,
@@ -24,11 +32,24 @@ class AjaxResponse
         return response()->json($out);
     }
 
+    /**
+     * [success 成功的信息]
+     * @author limx
+     * @param null $data 数据包
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function success($data = null)
     {
         return $this->ajaxResponse(1, $data);
     }
 
+    /**
+     * [fail 返回错误信息]
+     * @author limx
+     * @param $message 错误提示
+     * @param array $extra 额外的数据包
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function fail($message, $extra = [])
     {
         return $this->ajaxResponse(0, $extra, $message);
